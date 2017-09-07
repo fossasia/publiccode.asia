2f3570b14f5ef80663de559982d3ef5fc920dd07
max.mehl <max.mehl@fsfe.org>
2017-09-07 17:30:43 +0200

    some improvements with the config file split and build

--- site/build/build.sh
+++ site/build/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+TRANSLATIONS=
+
 basedir="${0%/*}/.."
 cd "$basedir"
 mode=$1
 
 # Unite static and language-specific config files to a single file
-cat config-static.toml languages/strings.*.toml > config.toml
+cat config-static.toml languages/strings.en.toml languages/strings.{de,fr}.toml > config.toml
 
 # Execute hugo buildrun
 if [ "$mode" == "server" ]; then
