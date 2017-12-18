a429a5bed37ccd035a0dfb8f3c0f0157b07d6cb1
Travis CI <>
2017-12-18 19:27:39 +0000

    Deploy to GitHub Pages: e4eb9e2a81dfb7de3625355d8158cda22982e5bf

--- js/onScrollMenu.js
+++ js/onScrollMenu.js
@@ -1,20 +1,17 @@
 //Script for making menu on scroll as it is on fossasia.org
 $(window).scroll(function() {
-		let languages = ["bg","de","eo","es","fr","gj","hi","id","xy","tl","tm","tr","vi","zh_tw"];
-		let path = $(window)[0].location.pathname.slice(1,3);
-		let translated = languages.includes(path);
+        let count = $(window)[0].location.pathname.split("/").length-2;
+        let lightLogoPath = "img/logo.svg";
+        let darkLogoPath = "img/logo_dark.svg";
+        for (var i=0; i<count; i++){
+            lightLogoPath = "../" + lightLogoPath;
+            darkLogoPath = "../" + darkLogoPath;
+        }
         if ($(window).scrollTop() > 1) {
             $('.navbar-default').addClass('changed-nav');
-            if(translated)
-            	$('#logo').replaceWith('<img id="logo" src="../img/logo_dark.svg" alt="Logo of the campaign" />');
-            else
-            	$('#logo').replaceWith('<img id="logo" src="img/logo_dark.svg" alt="Logo of the campaign" />');
+            $('#logo').replaceWith('<img id="logo" src=\"' + darkLogoPath + '\" alt="Logo of the campaign" />');
         } else {
             $('.navbar-default').removeClass('changed-nav');
-            if(translated)
-            	$('#logo').replaceWith('<img id="logo" src="../img/logo.svg" alt="Logo of the campaign" />');
-            else
-            	$('#logo').replaceWith('<img id="logo" src="img/logo.svg" alt="Logo of the campaign" />');
+            $('#logo').replaceWith('<img id="logo" src=\"' + lightLogoPath + '\" alt="Logo of the campaign" />');
         }
     });
-
